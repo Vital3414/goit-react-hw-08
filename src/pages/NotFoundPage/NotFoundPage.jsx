1ff4c525ab9b4b18e9aded5ed583b7c94ccc8448
Vital3414 <qwer1234432148@gmail.com>
2025-02-02 20:13:11 +0200

    finality

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+import css from "./NotFoundPage.module.css";
+
 const NotFound = () => {
-  return <h2>NotFound</h2>;
+  return (
+    <div className={css.container}>
+      <h2 className={css.title}>404</h2>
+      <p className={css.message}>Сторінку не знайдено</p>
+      <p className={css.description}>
+        Вибачте, але сторінка, яку ви шукаєте, не існує.
+      </p>
+      <Link to="/" className={css.link}>
+        Повернутися на головну
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
